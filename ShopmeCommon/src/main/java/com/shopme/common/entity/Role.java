6dd8993ef6e3e2bb301884a57eb93566d42f35c6
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2021-11-16 15:40:56 +0100

    adding role entity, role repository and unit test

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,57 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	private Integer id;
+	private String name;
+	private String description;
+
+	public Role() {
+	}
+
+	public Role(String name) {
+		this.name = name;
+	}
+
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getId() {
+		return id;
+	}
+
+	@Column(length = 40, nullable = false, unique = true)
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Column(length = 150, nullable = false)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
