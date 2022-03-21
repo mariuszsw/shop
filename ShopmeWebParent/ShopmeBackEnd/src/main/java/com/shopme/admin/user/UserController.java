381b9a15981764f7d58f76108fa66f137b7573ab
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-03-21 14:40:43 +0100

    create update user function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,6 +35,7 @@ public class UserController {
 		
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
 
 		return "user_form";		
 	}
@@ -48,4 +50,21 @@ public class UserController {
 		
 	}
 	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAtributes) {
+		try {			
+			User user = service.get(id);
+			List<Role> listRoles = service.listRoles();
+
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			model.addAttribute("listRoles", listRoles);
+
+			return "user_form";
+		} catch(UserNotFoundException ex) {
+			redirectAtributes.addFlashAttribute("message", ex.getMessage());
+			
+			return "redirect:/users";
+		}		
+	}	
 }
